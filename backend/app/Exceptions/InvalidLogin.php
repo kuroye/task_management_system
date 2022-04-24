44e7fb946810b5b87652dbf0c5298f67b78f7037
kuroye <erhanaket77@gmail.com>
2022-04-24 17:01:14 +0600

    login thing

---
+++ backend/app/Exceptions/InvalidLogin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class InvalidLogin extends Exception
+{
+    //
+    public function render()
+    {
+        return Response([
+            'message' => 'invalid login details',
+        ], 401);
+    }
+}
